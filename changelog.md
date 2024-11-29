b20e4e7c5d45d12edc7d66702629a3d93905f506
BlueBlock6 <66493630+blueblock6@users.noreply.github.com>
2024-11-28 19:04:42 -0500

    Fix disappearing checkpoints and visual bug

--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+# 1.0.5
+
+- Fix checkpoints disappearing when off screen in platformer mode and not returning (vanilla bug)
+- Fix visual bug with platformer checkpoints in normal mode
+
 # 1.0.4
 
 - 2.2074 Support
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,56 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/CCSprite.hpp>
 
 using namespace geode::prelude;
 
-std::unordered_map<CCObject*, GLubyte> opacities;
-
 class $modify(PlayLayer) {
+	struct Fields {
+		bool m_disabled;
+		std::unordered_map<CheckpointObject*, int> m_opacities;
+		CCNode* m_checkpointNode;
+	};
+
 	void updateCheckpoints() {
-		if(Mod::get()->getSettingValue<bool>("platformer-only") && !this->m_level->isPlatformer()) return;
-		opacities.clear();
+		m_fields->m_disabled = !m_isPracticeMode || (Mod::get()->getSettingValue<bool>("platformer-only") && !m_isPlatformer);
+		if(m_fields->m_disabled) return;
+
+		m_fields->m_opacities.clear();
 		int pos = m_checkpointArray->count();
-		CCObject* child;
-		CCARRAY_FOREACH(m_checkpointArray, child) {
-			auto checkpoint = static_cast<CheckpointObject*>(child)->m_physicalCheckpointObject;
+		for(auto checkpoint : CCArrayExt<CheckpointObject*>(m_checkpointArray)) {
 			pos--;
-			opacities[checkpoint] = std::ceil(Mod::get()->getSettingValue<int64_t>("default") * 2.55 * std::pow(Mod::get()->getSettingValue<double>("multiplier"), pos));
-			checkpoint->setOpacity(opacities[checkpoint]);
+			checkpoint->m_physicalCheckpointObject->m_isDisabled2 = false;
+			checkpoint->m_physicalCheckpointObject->m_ignoreFade = true;
+			m_fields->m_opacities[checkpoint] = std::ceil(Mod::get()->getSettingValue<int64_t>("default") * 2.55 * std::pow(Mod::get()->getSettingValue<double>("multiplier"), pos));
 		}
 	}
 
 	$override
 	void storeCheckpoint(CheckpointObject* p0) {
 		PlayLayer::storeCheckpoint(p0);
-		updateCheckpoints();
+		queueInMainThread([this]{ updateCheckpoints(); });
 	}
 
 	$override
 	void removeCheckpoint(bool p0) {
 		PlayLayer::removeCheckpoint(p0);
-		updateCheckpoints();
+		queueInMainThread([this]{ updateCheckpoints(); });
 	}
 
 	$override
-	void removeAllCheckpoints() {
-		PlayLayer::removeAllCheckpoints();
-		opacities.clear();
-	}
+	void postUpdate(float p0) {
+		PlayLayer::postUpdate(p0);
+		if(m_fields->m_disabled || m_checkpointArray->count() == 0) return;
 
-	$override
-	void startGame() {
-		PlayLayer::startGame();
-		opacities.clear();
-	}
-};
+		if(m_isPlatformer && !m_fields->m_checkpointNode) {
+			m_fields->m_checkpointNode = static_cast<CheckpointObject*>(m_checkpointArray->lastObject())->m_physicalCheckpointObject->getParent();
+			if(!m_fields->m_checkpointNode) return;
+		}
 
-class $modify(CCSprite) {
-	$override
-	void setOpacity(GLubyte opacity) {
-		if(opacities.contains(this)) opacity = opacity == 0 ? opacities[this] : opacity * opacities[this] / 255;
-		CCSprite::setOpacity(opacity);
+		for(auto checkpoint : CCArrayExt<CheckpointObject*>(m_checkpointArray)) {
+			checkpoint->m_physicalCheckpointObject->setOpacity(m_fields->m_opacities[checkpoint]);
+			if(!m_isPlatformer || checkpoint->m_physicalCheckpointObject->getParent()) continue;
+			m_fields->m_checkpointNode->addChild(checkpoint->m_physicalCheckpointObject);
+			checkpoint->m_physicalCheckpointObject->setVisible(true);
+		}
 	}
 };
